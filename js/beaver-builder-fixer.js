5dbde276d4d7d6e59a1f90e534c070943efdf630
marketividade <85942068+marketividade@users.noreply.github.com>
2021-08-06 12:26:48 +0100

    Update beaver-builder-fixer.js

--- js/beaver-builder-fixer.js
+++ js/beaver-builder-fixer.js
@@ -1,3 +1,4 @@
-	jQuery(".uabb-video-gallery-fancybox").attr("aria-label", "Click to watch the video");
-	jQuery(".uabb-video__play img").attr("alt", "Click to watch the video");
-	jQuery(".uabb-video-gallery-fancybox").attr("aria-label", "Click to watch the video");
+  console.log("beaver-builder-fixer.js is running");	
+jQuery(".uabb-video-gallery-fancybox").attr("aria-label", "Click to watch the video");
+jQuery(".uabb-video__play img").attr("alt", "Click to watch the video");
+jQuery(".uabb-video-gallery-fancybox").attr("aria-label", "Click to watch the video");
